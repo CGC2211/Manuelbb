05986116fb4d5d7281324e6da47f648aa0e9682e
Carlos Armando Gutierrez Cota <carlos.gtz.cota@hotmail.com>
2019-10-14 15:45:33 -0700

    Cambios chartjs

--- js/NP.js
+++ js/NP.js
@@ -25,6 +25,52 @@ $(document).ready(function(){
 function searchpaciente(){
       var paciente = $('#inputsearch').val();
 
-      
+
       $('.element-seguimiento-result').append()
    };
+
+function generargrafica(){
+
+  var dataArray = new Array();
+  var dataLabels = new Array();
+  var contador = 0;
+    $('.Peso').each(function(){
+      dataArray.push($(this).text());
+      contador++;
+      dataLabels.push(contador);
+      //check if data-layer already processed
+      /*if(!dataArray.indexOf(dataLayer))
+      {
+         //update data array
+         dataArray.push(dataLayer);
+         $('.popupDiv[data-layer="'+ dataLayer +'"]').each(function(){
+            //do your stuff here
+         });
+      }*/
+    });
+    
+    var ctx = document.getElementById('myChart');
+
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+                datasets: [{
+                  data: dataArray
+                }],
+                labels: dataLabels
+              },
+        options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Seguimiento de Peso' 
+                  },
+            legend: {
+                position : 'bottom'
+            }
+            }
+    });
+
+    console.log(dataArray);
+
+}   
